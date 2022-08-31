fd1ad3fd80cf38c5c561bac6695bc8cb7d5f6349
Sergei Medvedev <clip1988@yandex.ru>
2022-08-31 22:06:37 +0300

    Objectiv 50 project

---
+++ Objectiv50/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.Write("Введите позицию столбца: ");
+int columsPosition = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите позицию ряда: ");
+int rowsPosition = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Двумерный массив: ");
+int[,] matrix = new int[5, 5];
+GetArrayFillRandomNumbers(matrix);
+WriteArray(matrix);
+int element = 0;
+
+if (rowsPosition > matrix.GetLength(0) || columsPosition > matrix.GetLength(1))
+{
+    Console.WriteLine($"Такой позиции столбца {columsPosition} и позиции ряда {rowsPosition} нет !!!");
+}
+else
+{
+     element = matrix[rowsPosition - 1, columsPosition - 1];
+     Console.WriteLine($"На позиции столбца {columsPosition} и позиции ряда {rowsPosition} стоит значение элемента {element}");
+}
+void GetArrayFillRandomNumbers( int[,] array)
+{
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(-100, 100)/10;
+    }
+}
+}
+
+void WriteArray(int[,] array)
+{
+   for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine("");
+}
+}
