35cc72be361bec631607a575873998cb572e477c
Sergei Medvedev <clip1988@yandex.ru>
2022-08-31 22:45:21 +0300

    Objectiv 52 project

---
+++ Objectiv52/Program.cs
@@ -0,0 +1,45 @@
+﻿Console.Write("Введите размер столбца: ");
+int colums = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите размер ряда: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Двумерный массив: ");
+int[,] matrix = new int[colums, rows];
+GetArrayFillRandomNumbers(matrix);
+WriteArray(matrix);
+
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+    double avarage = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        avarage = (avarage + matrix[i,j]);
+    }
+    avarage = avarage / colums;
+    Console.WriteLine($"Среднее арифметическое столбца {j + 1}: {avarage + ";"}");
+}
+Console.WriteLine();
+
+void GetArrayFillRandomNumbers( int[,] array)
+{
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(-100, 100)/10;
+    }
+}
+}
+
+void WriteArray(int[,] array)
+{
+   for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine("");
+}
+}
+
+
